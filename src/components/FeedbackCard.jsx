49f9f47eeafe7dc0a7fa1e3fd4e715dd1d522c8d
mannie297 <dikson297@gmail.com>
2023-05-17 00:15:57 +0100

    fifth commit

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { quotes } from '../assets'
 
 const FeedbackCard = ({content, name, title, img}) => (
-  <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10
+  <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[350px] md:mr-5
   sm:mr-5 mr-0 my-5 feedback-card'>
     <img src={quotes} alt='dounle_quotes' className='w-[42px] 
     h-[27px] object-contain'/>
     
     <p className='font-poppins font-normal text-[18px]
-    leading-[32px] text-white my-10'>
+    leading-[32px] text-white my-10 mx-0'>
       {content}
     </p>
 
